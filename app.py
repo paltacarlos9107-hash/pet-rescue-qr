3c970094d33b4eebf9169da80bf8047d0bc4896d
Carlos Palta <paltacarlos9107@gmail.com>
2026-01-14 15:01:27 -0500

    Versión inicial

--- app.py
+++ app.py
@@ -179,6 +179,10 @@ def report_location():
 # -------------------------------------------------
 # EJECUTAR SERVIDOR
 # -------------------------------------------------
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     host = "0.0.0.0"
